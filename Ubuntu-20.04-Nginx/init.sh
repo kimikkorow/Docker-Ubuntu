4a64e3f44f96a03dd3d9148603d3f05b1edefb71
kimikkorow <36508192+kimikkorow@users.noreply.github.com>
2022-06-04 18:16:50 +0800

    update

--- Ubuntu-20.04-Nginx/init.sh
+++ Ubuntu-20.04-Nginx/init.sh
@@ -1,3 +1,17 @@
+echo "更新系统包"
+apt update 
+apt upgrade -y
+
+echo "设置时区"
+apt install -y tzdata 
+ln -fs /usr/share/zoneinfo/${TZ} /etc/localtime 
+echo ${TZ} > /etc/timezone 
+dpkg-reconfigure --frontend noninteractive tzdata
+
+echo "清理垃圾"
+apt autoremove -y
+apt autoclean -y
+
 echo "更换阿里源"
 echo "deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
 deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
@@ -10,20 +24,3 @@ deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe
 deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
 deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
 "> /etc/apt/sources.list
-
-echo "更新系统包"
-apt update 
-apt upgrade -y
-
-echo "设置时区"
-apt install -y tzdata 
-ln -fs /usr/share/zoneinfo/${TZ} /etc/localtime 
-echo ${TZ} > /etc/timezone 
-dpkg-reconfigure --frontend noninteractive tzdata
-
-echo "安装依赖"
-apt install sudo nano git wget curl -y
-
-echo "清理垃圾"
-apt autoremove -y
-apt autoclean -y
--- Ubuntu-20.04/init.sh
+++ Ubuntu-20.04/init.sh
@@ -1,16 +1,3 @@
-echo "更换阿里源"
-echo "deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
-# deb http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
-# deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
-"> /etc/apt/sources.list
-
 echo "更新系统包"
 apt update 
 apt upgrade -y
@@ -27,3 +14,16 @@ apt install sudo nano git wget curl -y
 echo "清理垃圾"
 apt autoremove -y
 apt autoclean -y
+
+echo "更换阿里源"
+echo "deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+# deb http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+# deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+"> /etc/apt/sources.list
