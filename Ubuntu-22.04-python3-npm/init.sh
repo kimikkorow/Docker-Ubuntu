74ca84ee1870c427e3dccf9427c17e3dfaf3d0da
kimikkorow <3337017542@qq.com>
2022-08-01 11:15:09 +0800

    Update init.sh

--- Ubuntu-22.04-python3-npm/init.sh
+++ Ubuntu-22.04-python3-npm/init.sh
@@ -10,7 +10,7 @@ dpkg-reconfigure --frontend noninteractive tzdata
 
 echo "安装依赖"
 apt install sudo -y
-sudo apt install nano git wget curl python3 npm make -y
+sudo apt install nano git wget curl python3 npm -y
 
 echo "清理垃圾"
 apt autoremove -y
