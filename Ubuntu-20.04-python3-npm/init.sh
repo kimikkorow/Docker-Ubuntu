eff39f32e33c2035d6663b45143cd6b46cb6472e
kimikkorow <3337017542@qq.com>
2022-09-05 16:09:14 +0800

    更换北交大源

--- Ubuntu-20.04-python3-npm/init.sh
+++ Ubuntu-20.04-python3-npm/init.sh
@@ -18,15 +18,17 @@ apt autoremove -y
 apt autoclean -y
 rm -rf /var/lib/apt/lists/*
 
-echo "更换阿里源"
-echo "deb https://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
-deb-src https://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
-deb https://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
-deb-src https://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
-deb https://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
-deb-src https://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
-# deb https://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
-# deb-src https://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
-deb https://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
-deb-src https://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
-" >/etc/apt/sources.list
+echo "使用北交大源"
+echo "# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb https://mirror.bjtu.edu.cn/ubuntu/ focal main restricted universe multiverse
+# deb-src https://mirror.bjtu.edu.cn/ubuntu/ focal main restricted universe multiverse
+deb https://mirror.bjtu.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
+# deb-src https://mirror.bjtu.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
+deb https://mirror.bjtu.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
+# deb-src https://mirror.bjtu.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
+deb https://mirror.bjtu.edu.cn/ubuntu/ focal-security main restricted universe multiverse
+# deb-src https://mirror.bjtu.edu.cn/ubuntu/ focal-security main restricted universe multiverse
+
+# 预发布软件源，不建议启用
+# deb https://mirror.bjtu.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse
+# deb-src https://mirror.bjtu.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse" >/etc/apt/sources.list
